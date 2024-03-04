eda70821cc697a5da0fb1e18fa52d8c42174c73e
Rahul Kumar <guptarahulboy123@gmail.com>
2024-03-04 23:59:39 +0530

    ready for use

---
+++ Web Caculator/script.js
@@ -0,0 +1,5 @@
+// 'calculate' function used to calculate entered values
+function calculate(){
+    var a=document.getElementById("display").value;
+    document.getElementById("display").value = eval(a);
+}
